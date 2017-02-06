dc275c4a8f8e91696e54d1c50acf5a4740e2e447
raghav <1raghuram3@gmail.com>
2017-02-06 19:18:29 +0530

    scn object is closed

--- assignment/assignment/src/com/Bridgelabz/assignment/LeapYear.java
+++ assignment/assignment/src/com/Bridgelabz/assignment/LeapYear.java
@@ -5,26 +5,32 @@ public class LeapYear
 	public static void main(String[] args)
 	{
 		Scanner scn=new Scanner(System.in);
-		System.out.println("enter the year");
-		int year=scn.nextInt();
-		 if((year/1000>=1)&&(year/1000<=9))
-		 {
-			 
-			 if ((year%4==0)||(year%400==0)&&(year%100!=0))
-			 {
-				 System.out.println("leap year");
-			 }
-			 else 
-			 {
-				 System.out.println("not a leap year");
-			 }
-		 }
-		 else 
-		 {
-			 System.out.println("number should be four digit");
-		 }
-	}
-	
-	
+		int n=0;
+		do
+		{
+			System.out.println("enter the year");
+			int
+			year=scn.nextInt();
+			if((year/1000>=1)&&(year/1000<=9))
+			{
 
+				if ((year%4==0)||(year%400==0)&&(year%100!=0))
+				{
+					System.out.println("leap year");
+				}
+				else 
+				{
+					System.out.println("not a leap year");
+				}
+			}
+			else 
+			{
+				System.out.println("number should be four digit");
+			}
+
+			System.out.println("enter 1 to continue");
+			n=scn.nextInt();
+			scn.close();
+		}while(n==1);
+	}
 }
---
+++ assignment/assignment/src/com/Bridgelabz/assignment/PowerOfTwo.java
@@ -0,0 +1,5 @@
+package com.Bridgelabz.assignment;
+
+public class PowerOfTwo {
+
+}
