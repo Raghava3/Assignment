45445cb5a97d5725cae63d017738b0d3e58fd761
raghav <1raghuram3@gmail.com>
2017-02-07 10:01:12 +0530

    excption is handeled

--- assignment/assignment/src/com/Bridgelabz/assignment/LeapYear.java
+++ assignment/assignment/src/com/Bridgelabz/assignment/LeapYear.java
@@ -1,16 +1,26 @@
 package com.Bridgelabz.assignment;
 import java.util.Scanner;
+import java.util.InputMismatchException;
 public class LeapYear 
 {
 	public static void main(String[] args)
 	{
 		Scanner scn=new Scanner(System.in);
 		int n=0;
+		int year=0;
 		do
 		{
-			System.out.println("enter the year");
-			int year=scn.nextInt();
+			try{
+				System.out.println("enter the year");
+				year=scn.nextInt();
+			}
+			catch(InputMismatchException  e)
+			{
+				System.out.println("entered value is not integer");
+				break;
+			}
 			if((year/1000>=1)&&(year/1000<=9))
+
 			{
 
 				if ((year%4==0)||(year%400==0)&&(year%100!=0))
@@ -20,19 +30,26 @@ public class LeapYear
 				else 
 				{
 					System.out.println("not a leap year");
+					
 				}
 			}
 			else 
 			{
 				System.out.println("number should be four digit");
 			}
-
+try{
 			System.out.println("enter 1 to continue");
 			n=scn.nextInt();
-			
+}
+catch(InputMismatchException e)
+{
+	System.out.println("Thank you");
+	System.exit(0);
+}
 		}while(n==1);
-	System.out.println("thank u");
+		System.out.println("thank u");
+		scn.close();
 	}
-	
-	
+
+
 }
